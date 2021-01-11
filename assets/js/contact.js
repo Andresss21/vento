176d40f00c6957ebccf69eaa6b425c903cdaf6f1
ronnielopez <ronnielopez503@gmail.com>
2021-01-11 16:50:33 -0600

    Contactos

---
+++ assets/js/contact.js
@@ -0,0 +1,37 @@
+AOS.init({
+    duration: 1200
+   });
+
+/**************** Menu ********************** */
+ const app = (() => {
+    let body;
+    let menu;
+    let menuItems;
+  
+    const init = () => {
+      body = document.querySelector('body');
+      menu = document.querySelector('.menu-icon');
+      menuItems = document.querySelectorAll('.nav-lista');
+  
+      applyListeners();
+    };
+  
+    const applyListeners = () => {
+      menu.addEventListener('click', () => toggleClass(body, 'nav-active'));
+    };
+  
+    const toggleClass = (element, stringClass) => {
+      if (element.classList.contains(stringClass))
+      element.classList.remove(stringClass);else
+  
+      element.classList.add(stringClass);
+    };
+  
+    init();
+  })();
+  
+  /****************************************** */
+
+   /**************** Ruleta ********************** */
+   
+   /****************************************** */
